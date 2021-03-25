697939aa890b80c0c5b9d4baded1c1ad1358177c
Ahren Swett <ahrenswett@gmail.com>
2021-03-24 21:19:05 -0700

    added fragment learning how to dynamicaly add fragments

--- ModMon/src/main/java/com/example/teslamodulemonitor/MainActivity.kt
+++ ModMon/src/main/java/com/example/teslamodulemonitor/MainActivity.kt
@@ -8,6 +8,7 @@ import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.teslamodulemonitor.R
 import com.example.teslamodulemonitor.addTestPack
 import java.io.File
@@ -27,11 +28,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-
-        renderPack("SOME KEY FOR INTENT")
+        renderPack()
     }
 
-    private fun renderPack(strExtra: String) {
+
+    private fun renderPack() {
         // Steps to decode/display packs from protocol buffer
         // 1. Get input stream or uri to file
         val file = File(filesDir, "protoOut")
@@ -48,6 +49,7 @@ class MainActivity : AppCompatActivity() {
             // 3. Set fields into View
             val textView = findViewById<TextView>(R.id.textViewDecode)
             // pass textView and Pack to render function
+
             displayPackValues(textView, pack)
         }
     }
@@ -62,11 +64,16 @@ class MainActivity : AppCompatActivity() {
         // Handle the returned Uri
     //need an activity that shows pack module and cell values
 
-    fun displayPackValues(textView: TextView, pack: Test.Pack){
+    fun displayPackValues(textView: TextView, pack: Test.Pack, ){
+        val vhf= findViewById<ConstraintLayout>(R.id.lon)
+        vhf.findViewById<TextView>(R.id.nameOfHeldValue).setText("Pack Voltage")
+        vhf.findViewById<TextView>(R.id.value).setText("${pack.currentVoltage}")
+
         var str = StringBuilder()
         str.append("Pack name: ${pack.packName}\nPack Voltage:${pack.currentVoltage}\nPack Temp:${pack.averagePacktemp}\n"
                 + "Number of modules in ${pack.packName} is: ${pack.numberOfModules}\n")
         for (mod in pack.modulesList){
+//            val
             str.append("Module ${mod.id} is ${mod.moduleVoltage}V and ${mod.moduleTemp}DegC\n" +
                     "Highest voltage cell is ${mod.highestCellVolt}V\n" +
                     "Lowest voltage cell is ${mod.lowestCellVolt}V \n")
--- ModMon/src/main/java/com/example/teslamodulemonitor/ValueHolderFrag.kt
+++ ModMon/src/main/java/com/example/teslamodulemonitor/ValueHolderFrag.kt
@@ -13,14 +13,14 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [VoltFrag.newInstance] factory method to
+ * Use the [ValueHolderFrag.newInstance] factory method to
  * create an instance of this fragment.
  */
-class VoltFrag : Fragment() {
+class ValueHolderFrag : Fragment() {
     // TODO: Rename and change types of parameters
 
-    private var param1: String? = null
-    private var param2: String? = null
+    private var param1: String? = "llama"
+    private var param2: String? = "25"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +33,7 @@ class VoltFrag : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_volt, container, false)
+        return inflater.inflate(R.layout.fragment_valueholder, container, false)
     }
 
     companion object {
@@ -48,7 +48,7 @@ class VoltFrag : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-                VoltFrag().apply {
+                ValueHolderFrag().apply {
                     arguments = Bundle().apply {
                         putString(ARG_PARAM1, param1)
                         putString(ARG_PARAM2, param2)
