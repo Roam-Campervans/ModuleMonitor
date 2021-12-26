b774fe89a06b5860dbcfd19691985c46b8c96c33
Ahren Swett <ahrenswett@gmail.com>
2021-12-26 14:31:26 -0800

    Trouble getting into recycler View

--- ModMon/src/main/java/com/example/teslamodulemonitor/PackAdapter.kt
+++ ModMon/src/main/java/com/example/teslamodulemonitor/PackAdapter.kt
@@ -6,27 +6,31 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class PackAdapter(private val packs: ArrayList<Test.Pack>) :
-        RecyclerView.Adapter<PackAdapter.PackViewHolder>() {
+class PackAdapter(packs: ArrayList<Test.Pack>) : RecyclerView.Adapter<PackAdapter.PackViewHolder>() {
+    var packs = packs
 
     /**
      * Provide a reference to the type of views that you are using
      * (custom ViewHolder).
      */
     class PackViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        lateinit var packFrag : PackFrag
+        lateinit var pack: Test.Pack
+        lateinit var packFrag: PackFrag
+
 
         init {
 //           TODO: Define click listener for the ViewHolder's View.
 //            Define fillable fields and fragments
+         }
+
+        fun bindPack(pack: Test.Pack){
+            this.pack = pack
+            packFrag = PackFrag.newInstance(pack.packName,pack.currentVoltage,pack.averagePacktemp)
+
 
         }
-    }
 
-    abstract class BaseViewHolder(view: View): RecyclerView.ViewHolder(view){
-        fun onBind(test: Test.Pack) {
 
-        }
     }
 
     // Create new views (invoked by the layout manager)
@@ -36,15 +40,13 @@ class PackAdapter(private val packs: ArrayList<Test.Pack>) :
         return PackViewHolder(view)
     }
 
-
-
     // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(packViewHolder: PackViewHolder, position: Int) {
-
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
-        var pack = packs[position]
-        packViewHolder.packFrag = PackFrag.newInstance(pack.packName,pack.currentVoltage,pack.averagePacktemp)
+        packViewHolder.bindPack(packs[position])
+       //Need to add it but no FragmentManager :(
+
     }
 
     // Return the size of your dataset (invoked by the layout manager)
